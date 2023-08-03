e1e315952ce9a53a53d473a25e5d58b4281823a0
suchlikesamsung <ncryko1@gmail.com>
2023-08-03 22:36:55 +0900

    8월 3일 공부

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Expenseitem from "./components/Expenseitem";
 
 function App() {
+  const expense = [
+    {
+      title: 'Car Insurance1', amount: 294, date: new Date(2021, 3, 28)
+    },
+    {
+      title: 'Car Insurance2', amount: 124, date: new Date(2021, 4, 28)
+    }
+    ,
+    {
+      title: 'Car Insurance3', amount: 456, date: new Date(2021, 5, 28)
+    }
+    , {
+      title: 'Car Insurance4', amount: 1231, date: new Date(2021, 6, 28)
+    }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenseitem/>
+      <Expenseitem
+        data={expense[0]}/>
+      <Expenseitem
+        data={expense[1]}/>
+      <Expenseitem
+      data={expense[2]}/>
+
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,10 +1,11 @@
 import './ExpenseItem.css'
+
 function addComma(number){
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
 
 
-function Expenseitem() {
+function Expenseitem(props) {
     const expenseDate = new Date(2021,2,28);
     const expenseValue = 19012
     const transValue = addComma(expenseValue)
@@ -12,10 +13,10 @@ function Expenseitem() {
 
     return <>
         <div className="expense-item">
-            <div>{expenseDate.toISOString().replace('T',' ')}</div>
+            <div>{props.data.date.toISOString()}</div>
             <div className="expense-item__description ">
-                <h2>가격</h2>
-                <div className="expense-item__price">{transValue}</div>
+                <h2>{props.data.title}</h2>
+                <div className="expense-item__price">{props.data.amount}</div>
             </div>
         </div>
     </>
