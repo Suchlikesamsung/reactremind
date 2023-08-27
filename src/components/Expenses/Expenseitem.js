f50a12930ee3e6f0d53cb9757603d302eeffa1e7
suchlikesamsung <ncryko1@gmail.com>
2023-08-27 23:25:18 +0900

    2023-08-27 공부

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -12,14 +12,19 @@ function Expenseitem(props) {
     // const expenseDate = new Date(2021,2,28);
     // const expenseValue = 19012
     // const transValue = addComma(expenseValue)
+    let title = props.title;
+    const clickHandler = () => {
+        title = '됫나용?'
+    }
     console.log(props)
     return <>
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description ">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>change Title</button>
         </Card>
     </>
 }
